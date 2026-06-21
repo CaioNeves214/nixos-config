fa9fa564a85a43086ac731d887f24b8248d7993f
CaioNeves214 <workspace2144@gmail.com>
2026-06-21 16:15:11 -0300

    Criação do esboço inicial da waybar

--- modules/home/packages.nix
+++ modules/home/packages.nix
@@ -7,9 +7,12 @@
      fastfetch # Fastfecth
      waybar # TaskBar
      rofi # Launcher program
-     hyprpaper # Wallpaper 
+     hyprpaper # Wallpaper
      xfce.thunar # File Explorer
      mako # ? (find out)
      discord # voice channel
+     gsimplecal # Calendário popup (abre ao clicar no relógio)
+     brightnessctl # Controle de brilho via scroll no módulo backlight
+     pavucontrol # Mixer de áudio (abre ao clicar no volume)
    ];
  }
